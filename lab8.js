234df2f9e9d1f3998ecc8f531fc2a37f00db6f54
Студент Факультета Бизнеса <student@fb.local>
2023-06-06 16:07:45 +0700

    Количество дней с даты рождения

--- lab8.js
+++ lab8.js
@@ -7,3 +7,20 @@ function showDate(){
     out.innerHTML += 'Дата и время для локали Уэльса:  ' + today.toLocaleString('cy-GB')+ '<br>';
     out.innerHTML += 'Дата и время для локали Гонконга: ' + today.toLocaleString('zh-HK')+ '<br>';
 }
+
+function showDaysCount(){
+    let today = new Date();
+    let inputDate = document.querySelector('input[type=date]');
+    let birthday = new Date(inputDate.value);
+    let daysCount = (today - birthday)/1000/60/60/24;
+    daysCount = Math.floor(daysCount);
+    let output = document.getElementById('result');
+    output.innerHTML = 'Количество дней с даты рождения: ' + daysCount;
+}
+
+function cleanDaysCount() {
+    let inputDate = document.querySelector('input[type=date]');
+    inputDate.value = '';
+    let output = document.getElementById('result');
+    output.innerHTML = '';
+}
